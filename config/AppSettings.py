1fd50dc5cd3ae7746de930f2df0c9b2d12cad62f
Krystian <sitarz.code@gmail.com>
2025-11-02 00:12:13 +0100

    Adding env variables

--- config/AppSettings.py
+++ config/AppSettings.py
@@ -1,3 +1,5 @@
+import os
+
 class AppSettings:
     api_key = None
     start_from_id = None
@@ -8,6 +10,6 @@ class AppSettings:
         import json
         with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
-            AppSettings.api_key = data['apiKey']
-            AppSettings.start_from_id = data.get('startFromId')
-            AppSettings.stop_at_id = data.get('stopAtId')
+            AppSettings.api_key = data['apiKey'] if os.getenv('API_KEY') is None else os.getenv('API_KEY')
+            AppSettings.start_from_id = data.get('startFromId') if os.getenv('START_FROM_ID') is None else os.getenv('START_FROM_ID')
+            AppSettings.stop_at_id = data.get('stopAtId') if os.getenv('STOP_AT_ID') is None else os.getenv('STOP_AT_ID')
